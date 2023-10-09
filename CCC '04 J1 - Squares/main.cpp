99be6d7736aad359359bbcd8982c3f89d57a379f
Georgios D-V <47365973+Sistav@users.noreply.github.com>
2023-10-09 11:42:07 -0400

    Solved problems in CCC '04 J1 - Squares

---
+++ CCC '04 J1 - Squares/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int numero;
+    cin >> numero;
+    cout << "The largest square has side length " << (int) sqrt(numero) << "." << endl;
+    return 0;
+}
