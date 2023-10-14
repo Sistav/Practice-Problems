e0fd7159194da5b341f64422ecfa7f45cfbe7c40
Georgios D-V <47365973+Sistav@users.noreply.github.com>
2023-10-14 11:42:19 -0400

    Solved problems in Sum of Primes 2

---
+++ Sum of Primes 2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; ++i) {
+        int min,max;
+        cin >> min >> max;
+
+        bool aaaaaaaa = 0;
+        for (int i = begin; i < end; i = i+2)
+        {
+            isPrime = 1;
+            for (int j = 2; j<i; j++)
+                if (i % j == 0)
+                {
+                    aaaaaaaa = 0;
+                    break;
+                }
+            if (aaaaaaaa)
+                file << i << " \n";
+        }
+    }
+}
