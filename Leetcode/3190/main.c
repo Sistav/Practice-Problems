c5ab263deb3c2e738ac8c44775bd9d3e10db0ca2
Georgios D-V <47365973+Sistav@users.noreply.github.com>
2024-10-15 12:32:51 -0400

    Trolling 3190

---
+++ Leetcode/3190/main.c
@@ -0,0 +1,9 @@
+    int minimumOperations(int* nums, int numsSize) {
+        int ret = 0;
+        for (int i =0; i < numsSize; i++){
+            if (nums[i] % 3 != 0){
+                ret++;
+            }
+        }
+        return ret;
+    }
